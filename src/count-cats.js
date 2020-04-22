daf32f05fb4b400624540ff700880ddec79fbee2
Andrei <55841390+svatukhin@users.noreply.github.com>
2020-04-22 15:38:35 +0300

    change count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,4 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(backyard) {
+  throw 'Not implasdadademented';
+  // remove line with error and write your code her sdasd
 };
